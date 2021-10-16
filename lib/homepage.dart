c601f99e062ab9844e6bf7f33da275a21c67c436
tsaniarisqi <tsaniarisqi98@gmail.com>
2021-10-16 21:19:23 +0700

    date format

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,12 +12,33 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:HexColor("#109d58") ,
+        backgroundColor: HexColor("#109d58"),
         title: Text("Hydro System"),
       ),
-      body: Container(
-        color: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.only(top: 10, left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              getCurrentTime(),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+            ),
+            Text(
+              getCurrentDate(),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+getCurrentDate() {
+  return DateFormat('EEE, dd MMM yyyy').format(DateTime.now());
+}
+
+getCurrentTime() {
+  return DateFormat('kk:mm').format(DateTime.now());
 }
