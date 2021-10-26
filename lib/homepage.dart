8087abd368ce59475b8e4043b5f1389587f76d8f
tsaniarisqi <tsaniarisqi98@gmail.com>
2021-10-27 00:04:16 +0700

    memperbaiki halaman home

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -13,180 +13,225 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: HexColor("#109d58"),
-        title: Text("Hydro System"),
+        elevation: 0,
+        title: Text("Hydro"),
       ),
-      body: Padding(
-        padding: EdgeInsets.only(top: 10, left: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              getCurrentTime(),
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
-            ),
-            Text(
-              getCurrentDate(),
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 40, left: 7),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    height: 195,
-                    width: 140,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(25),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.1),
-                          spreadRadius: 5,
-                          blurRadius: 5,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(top: 30),
-                          child: Text(
-                            "pH",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 25),
-                          child: Image.asset(
-                            'assets/images/ph.png',
-                            width: 50,
-                            height: 50,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 22),
-                          child: Text(
-                            "7,3",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 30,
-                  ),
-                  Container(
-                    height: 195,
-                    width: 140,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(25),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.1),
-                          spreadRadius: 5,
-                          blurRadius: 5,
-                          offset: Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(top: 30),
-                          child: Text(
-                            "Nutrisi",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 25),
-                          child: Image.asset(
-                            'assets/images/flask.png',
-                            width: 50,
-                            height: 50,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(top: 22),
-                          child: Text(
-                            "750 ppm",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 18),
+            width: double.infinity,
+            height: 280,
+            decoration: BoxDecoration(
+              color: HexColor("#109d58"),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(70),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.only(top: 40, left: 85),
-              child: Container(
-                height: 195,
-                width: 140,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.1),
-                      spreadRadius: 5,
-                      blurRadius: 5,
-                      offset: Offset(0, 3), // changes position of shadow
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  getCurrentTime(),
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  getCurrentDate(),
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset(
+                      'assets/images/header.png',
+                      width: 214,
+                      height: 214,
                     ),
                   ],
                 ),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(top: 30),
-                      child: Text(
-                        "Suhu",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 18, top: 310, right: 18),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Plant Overview",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      height: 145,
+                      width: 110,
+                      decoration: BoxDecoration(
+                        color: HexColor("#F8E752"),
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.1),
+                            spreadRadius: 5,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 10),
+                            child: Text(
+                              "pH",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 18),
+                            child: Image.asset(
+                              'assets/images/ph.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 15),
+                            child: Text(
+                              "7,3",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20),
-                      child: Image.asset(
-                        'assets/images/celsius.png',
-                        width: 50,
-                        height: 50,
+                    SizedBox(width: 22),
+                    Container(
+                      height: 145,
+                      width: 110,
+                      decoration: BoxDecoration(
+                        color: HexColor("#F8E752"),
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.1),
+                            spreadRadius: 5,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 10),
+                            child: Text(
+                              "pH",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 18),
+                            child: Image.asset(
+                              'assets/images/flask.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 15),
+                            child: Text(
+                              "750 ppm",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 22),
-                      child: Text(
-                        "25 \u2103",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    SizedBox(width: 22),
+                    Container(
+                      height: 145,
+                      width: 110,
+                      decoration: BoxDecoration(
+                        color: HexColor("#F8E752"),
+                        borderRadius: BorderRadius.circular(25),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.1),
+                            spreadRadius: 5,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 10),
+                            child: Text(
+                              "Suhu",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 18),
+                            child: Image.asset(
+                              'assets/images/celsius.png',
+                              width: 50,
+                              height: 50,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top: 15),
+                            child: Text(
+                              "25 \u2103",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-              ),
-            )
-          ],
-        ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
